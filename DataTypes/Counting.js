6e87088d9efaba1b6130dcf89fbd1f2658ed4eee
Polina Korolchuk <korolchuuuk@mail.ru>
2025-06-16 01:59:49 +0300

    fix: add in one folder

--- DataTypes/Counting.js
+++
@@ -1,9 +0,0 @@
-//Напишите функцию, которая принимает число и выводит количество цифр в этом числе
-
-function counting(num) {
-    const strNum = Math.abs(num).toString();
-    return strNum.length;
-}
-
-const result = counting(12345);
-console.log(result);
